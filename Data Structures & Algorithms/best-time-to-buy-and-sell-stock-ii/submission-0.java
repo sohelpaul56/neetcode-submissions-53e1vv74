c981e6183e77e55f08c51e076aeb2a71a07ce70c
SOHEL PAUL <sohelpaul56@gmail.com>
2026-06-15 10:22:22 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int i=0;
+        int j=1;
+        int res=0;
+        while(j<prices.length){
+        if((prices[j]-prices[i])>0) res+=prices[j++]-prices[i++];
+        else{
+        i++;
+        j++;
+        }
+        }
+        return res;
+    }
+}
